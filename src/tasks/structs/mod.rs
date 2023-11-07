4b89240122ae8312c621a04045589fddbd7f4251
Stephen Jones <stephenj99642@gmail.com>
2023-11-07 22:12:15 +0000

    Moving Duration and Due into task struct module

--- src/tasks/structs/mod.rs
+++ src/tasks/structs/mod.rs
@@ -1,11 +1,5 @@
 /// Defines the `Task` struct and associated functions.
 pub mod task;
 
-/// Defines the `Due` struct, which represents the due date/time of a task.
-pub mod due;
-
-/// Defines the `Duration` struct, which represents the duration of a task.
-pub mod duration;
-
 /// Defines the `NewTask` struct and its `upload()` function.
 pub mod new_task;
--- src/tasks/structs/task.rs
+++ src/tasks/structs/task.rs
@@ -1,6 +1,4 @@
 use serde::Deserialize;
-use crate::tasks::structs::due::Due;
-use crate::tasks::structs::duration::Duration;
 
 /// Defines a task as returned by the API. There is no meaning in constructing a task like this
 /// directly (make a `NewTask` or `UpdateTask` struct to make those API calls). Instances of this
@@ -28,3 +26,43 @@ pub struct Task {
     pub assigner_id: Option<String>,
     pub duration: Option<Duration>,
 }
+
+
+/// Represents the duration of a task. The possible `unit` values are "minute" or "day".
+/// The "amount" field is the number of minutes or days, depending on the unit.
+///
+/// If creating a new task, use NewDuration instead.
+#[derive(Debug, Deserialize)]
+#[allow(missing_docs)]
+pub struct Duration {
+    pub amount: u32,
+    pub unit: String,
+}
+
+
+/// Represents the due date of a task deserialized from the API.
+///
+/// If creating a new task, use `NewDue` instead.
+#[derive(Debug, Deserialize)]
+pub struct Due {
+    /// The string form of the due date, as entered by a user. Its format is arbitrary and user-
+    /// selected.
+    pub string: String,
+
+    /// A date in the format `YYYY-MM-DD`, corrected to the user's timezone.
+    pub date: String,
+
+    /// Whether the task has a recurring due date. If `is_recurring` is `true`, then when this task
+    /// is completed, the next instance of the task will be created with a due-date calculated based
+    /// on the natural-language content of the `string` field.
+    pub is_recurring: bool,
+
+    /// Only returned if an exact due time is set. Conforms to
+    /// [RFC3339](https://www.ietf.org/rfc/rfc3339.txt) format.
+    pub datetime: Option<String>,
+
+    /// Only returned if an exact due time is set. It will either be in an IANA Timezone Database
+    /// format (e.g. "Europe/London"), or a UTC offset format ("UTC±HH:MM").
+    pub timezone: Option<String>,
+}
+
