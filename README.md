aca4de4368489dd0d435ba57787aa1b3eafa8bef
Stephen Jones <stephenj99642@gmail.com>
2023-11-10 16:52:47 +0000

    Functions for handling shared labels

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ but sections, comments, and labels are not. I expect everything to be fully impl
 
 See https://developer.todoist.com/rest/v2/ for more information on the Todoist REST API, or
 https://docs.rs/rustodoist_rest2/latest/rustodoist_rest2/ for the documentation for this crate.
+
+Please report any bugs on the GitHub repository at 
--- src/labels/mod.rs
+++ src/labels/mod.rs
@@ -7,6 +7,15 @@
 //! - To create a new label, make a `NewLabel` struct and call `upload()` on it
 //! - To update a label whose ID you know, make an `UpdateLabel` struct and call `update()` on it
 //! - To delete a label whose ID you know, run `delete_label_by_id()`
+//!
+//! In addition, Todoist has the concept of shared labels. The information about these is less
+//! sophisticated than for personal labels; you can get a list of all of them, rename one or
+//! delete one. There is no `struct` for shared labels, as they're just represented as a string.
+//! They also have no ID.
+//!
+//! - To get a list of all shared labels, use `get_all_shared_labels()`
+//! - To rename a shared label, use `rename_shared_label()`
+//! - To remove a shared label from all tasks to which it is applied, use `delete_shared_label()`
 
 
 mod structs;
@@ -46,6 +55,47 @@ pub fn delete_label_by_id(user: &TodoistUser, id: &str) -> Result<(), TodoistAPI
     let response = client
         .delete(format!("https://api.todoist.com/rest/v2/labels/{}", id))
         .header("Authorization", String::from("Bearer ") + &user.token)
+        .send();
+    get_204_from_reqwest_response(response)
+}
+
+
+/// Get a Vec of all the user's shared labels, just as strings.
+pub fn get_all_shared_labels(user: &TodoistUser) -> Result<Vec<String>, TodoistAPIError> {
+    // Send the API request
+    let client = reqwest::blocking::Client::new();
+    let response = client
+        .get("https://api.todoist.com/rest/v2/labels/shared")
+        .header("Authorization", String::from("Bearer ") + &user.token)
+        .query(&[("omit_personal", "false")])
+        .send();
+    get_from_reqwest_response(response)
+}
+
+
+/// Rename a shared label.
+pub fn rename_shared_label(user: &TodoistUser, old_name: &str, new_name: &str) -> Result<(), TodoistAPIError> {
+    // Send the API request
+    let client = reqwest::blocking::Client::new();
+    let response = client
+        .post("https://api.todoist.com/rest/v2/labels/shared/rename")
+        .header("Authorization", String::from("Bearer ") + &user.token)
+        .header("Content-Type", "application/json")
+        .body(format!("{{\"old_name\": \"{}\", \"new_name\": \"{}\"}}", old_name, new_name))
+        .send();
+    get_204_from_reqwest_response(response)
+}
+
+
+// Remove a shared label from all tasks to which it is applied
+pub fn delete_shared_label(user: &TodoistUser, name: &str) -> Result<(), TodoistAPIError> {
+    // Send the API request
+    let client = reqwest::blocking::Client::new();
+    let response = client
+        .post("https://api.todoist.com/rest/v2/labels/shared/remove")
+        .header("Authorization", String::from("Bearer ") + &user.token)
+        .header("Content-Type", "application/json")
+        .body(format!("{{\"name\": \"{}\"}}", name))
         .send();
     get_204_from_reqwest_response(response)
 }
