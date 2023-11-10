376ddf2d1df713ceb12ef938f006141eb177185f
Stephen Jones <stephenj99642@gmail.com>
2023-11-10 23:25:37 +0000

    Remember Sections needs to be deserialised

--- src/sections/structs/section.rs
+++ src/sections/structs/section.rs
@@ -1,5 +1,7 @@
+use serde::Deserialize;
 /// Defines a Todoist Section. There's no need to create one of these youself, but
 /// instances of this are returned from most section-related API calls.
+#[derive(Debug, Deserialize)]
 pub struct Section {
     pub id: String,
     pub project_id: String,
