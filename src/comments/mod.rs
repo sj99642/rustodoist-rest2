2163708a25889b1baa52c937fdb111c190b4f17c
Stephen Jones <stephenj99642@gmail.com>
2023-11-21 22:39:12 +0000

    Functions to delete and update comments

--- src/comments/mod.rs
+++ src/comments/mod.rs
@@ -18,7 +18,7 @@ mod structs;
 pub use crate::comments::structs::comment::Comment;
 pub use crate::comments::structs::attachment::{self, Attachment};
 use crate::err::TodoistAPIError;
-use crate::general::get_from_reqwest_response;
+use crate::general::{get_204_from_reqwest_response, get_from_reqwest_response};
 
 use crate::TodoistUser;
 
@@ -55,6 +55,35 @@ pub fn get_comment_by_id(user: &TodoistUser, comment_id: &str) -> Result<Comment
     let response = client
         .get(format!("https://api.todoist.com/rest/v2/comment/{}", comment_id))
         .header("Authorization", String::from("Bearer ") + &user.token)
+        .send();
+    get_from_reqwest_response(response)
+}
+
+/// Delete a comment with the given ID
+pub fn delete_comment_by_id(user: &TodoistUser, comment_id: &str) -> Result<(), TodoistAPIError> {
+    // Send the API request
+    let client = reqwest::blocking::Client::new();
+    let response = client
+        .delete(format!("https://api.todoist.com/rest/v2/comments/{}", comment_id))
+        .header("Authorization", String::from("Bearer ") + &user.token)
+        .send();
+    get_204_from_reqwest_response(response)
+}
+
+
+/// Update the text of the specified comment
+pub fn update_comment_content(user: &TodoistUser, comment_id: &str, new_content: &str) -> Result<Comment, TodoistAPIError> {
+    // Create the JSON body
+    let body = format!("{{\"content\": \"{}\"}}", new_content);
+
+    // Send the API request
+    let client = reqwest::blocking::Client::new();
+    let response = client
+        .post(format!("https://api.todoist.com/rest/v2/comments/{}", comment_id))
+        .header("Authorization", String::from("Bearer ") + &user.token)
+        .header("Content-Type", "application/json")
+        .header("X-Request-Id", uuid::Uuid::new_v4().to_string())
+        .body(body)
         .send();
     get_from_reqwest_response(response)
 }
